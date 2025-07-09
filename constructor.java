780a04100f0d824a4c153c8b4b02723f293f92ff
akshatha12-dev <maheshasmahesha8@gmail.com>
2025-07-09 03:23:53 -0700

    Add files via upload

---
+++ constructor.java
@@ -0,0 +1,23 @@
+class xyz
+{
+int length,width;
+xyz (int x, int y) //constructor
+{
+length=x;
+width=y;
+}
+function()
+{
+int a, b;
+a=40;
+b=100;
+}
+}
+class xyz
+{
+public static void main(String args[])
+{
+xyz obj=new xyz(40,100);
+obj.function();
+}
+}
